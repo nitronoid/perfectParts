2007542a03bbeabc2bc1d395d4cd5d7b39b493a6
s4902673 <jackdiver@hotmail.co.uk>
2017-05-15 17:16:05 +0100

    Fixed opengl includes and SDL

--- include/Scene.h
+++ include/Scene.h
@@ -1,7 +1,7 @@
 #ifndef SCENE_H
 #define SCENE_H
 
-#include <SDL2/SDL.h>
+#include <SDL.h>
 #include <string>
 #include <glm/glm.hpp>
 #include "Emitter.h"
--- include/pngutils.h
+++ include/pngutils.h
@@ -1,7 +1,13 @@
 #ifndef PNGUTILS_H
 #define PNGUTILS_H
 
+#if defined (__linux__) || defined (WIN32)
 #include <GL/gl.h>
+#include <GL/glu.h>
+#else
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#endif
 
 /// @note modified using pnglib code to allow saving a png
 /// [Accessed 2017]. Available from: "http://www.labbookpages.co.uk/software/imgProc/libPNG.html"
